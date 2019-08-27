4d88773ece4921c39b4a46e2e99025ac4ce69a76
manu.joseph <manu.joseph@cubettech.com>
2019-08-27 10:47:16 +0530

    Conditional Rendering

--- src/index.js
+++ src/index.js
@@ -1,53 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Inputs from './Inputs';
-import Faker from 'faker';
-import Card from './card'
 
-class Former extends React.Component
+class Starter extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            lat:null,
+            errorMsg:null
+        }
+
+        navigator.geolocation.watchPosition(
+            (position) => {
+                this.setState({
+                    lat:position.coords.latitude
+                })
+            },
+            (err) => {
+                console.log(err.message);    
+                this.setState({
+                    errorMsg:err.message
+                })
+            });
+    }
     render()
     {
-        return (
-                <form method="post">
-                <Card>
-                    <h2>Warning</h2>
-                    Aree you sure about this??
-                </Card>
-                    <Card>
-                        <Inputs 
-                            name="Manu Joseph" 
-                            alt="Manu Avatar" 
-                            img={Faker.image.avatar()} 
-                            func={this.firstNamer} 
-                            blog="Hello Friends Welcome to Manus World"
-                        />
-                    </Card>
-                    <Card>
-                        <Inputs 
-                            name="Mahith Mangal" 
-                            alt ="Mahith Avatar" 
-                            img={Faker.image.avatar()} 
-                            func={this.lastNamer} 
-                            blog="this is mahith area, please move out"
-                        />
-                    </Card>
-                </form>
+        if(this.state.lat && !this.state.errorMsg)
+        {
+            return <div> <h2>Lattiude: {this.state.lat} </h2><br/></div>
+        }
+        if(!this.state.lat && this.state.errorMsg)
+        {
+            return (
+                <div>
+                     Error: <font color="red">{this.state.errorMsg}</font>
+                </div>
             );
-    }
+        }
 
-    firstNamer = (e) =>
-    {
-        alert("First namer is working")
-    }
+        return (
+            <div> <font color="green">Loading . ..</font></div>
+        );
 
-    lastNamer = (e) =>
-    {
-        alert("Last namer is working")
     }
 }
 
+
 ReactDOM.render(
-    <Former/>,
+    <Starter/>,
     document.getElementById('root')
-)
+    );
